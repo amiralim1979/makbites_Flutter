f840d72915119a0e4abbb97093e16f19858fb17f
amiralim1979 <amir.aghai1979@gmail.com>
2025-01-25 10:37:23 +0330

    Update Welcome_screen.dart

--- Welcome_screen.dart
+++ Welcome_screen.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MAKApp());
-}
-
-class MAKApp extends StatelessWidget {
-  const MAKApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-    );
-  }
-}
+import 'free_menu_screen.dart';
+import 'premium_menu_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
@@ -22,9 +8,34 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF2B4D3C), // رنگ پس‌زمینه
       body: Center(
-        child: Text(
-          "Welcome to MAK Bites!",
-          style: TextStyle(color: Colors.white, fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome to MAK Bites!",
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FreeMenuScreen()),
+                );
+              },
+              child: Text("Free Account"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PremiumMenuScreen()),
+                );
+              },
+              child: Text("Premium Account"),
+            ),
+          ],
         ),
       ),
     );
