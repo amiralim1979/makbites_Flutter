9e478e0c6b5c9bd314fe62613d07bf36eb0d2cef
amiralim1979 <amir.aghai1979@gmail.com>
2025-01-27 22:40:49 +0330

    Create auth_service.dart

---
+++ auth_service.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // ثبت‌نام کاربر
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // ورود کاربر
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // خروج کاربر
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  // گرفتن اطلاعات کاربر فعلی
+  Future<User?> getCurrentUser() async {
+    return _auth.currentUser;
+  }
+}
